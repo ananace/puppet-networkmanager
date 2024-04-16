4becea7569484684c940e750009d7b5c942eeb91
Alexander Olofsson <alexander.olofsson@liu.se>
2024-04-16 16:10:44 +0200

    Skip NM status if daemon is not running

--- lib/facter/networkmanager.rb
+++ lib/facter/networkmanager.rb
@@ -12,6 +12,8 @@ Facter.add(:networkmanager) do
     version = Facter::Core::Execution.execute("#{@nm_cmd} --version").strip
     status = Facter::Core::Execution.execute("#{@nmcli_cmd} general status", on_fail: nil)&.strip&.split("\n")
 
+    status = nil if status&.first =~ /NetworkManager is not running/
+
     status = Hash[status.first.split.map(&:downcase).zip(status.last.split)] unless status.nil?
     status ||= {}
 
