888263a38f03fe1770d95d46197051fd905ce8c4
Ulrik Haugen <ulrik.haugen@liu.se>
2024-05-08 14:05:28 +0200

    Permit disabled for ip6_method

--- manifests/connection.pp
+++ manifests/connection.pp
@@ -16,7 +16,7 @@ define networkmanager::connection (
   Optional[Boolean] $ip4_may_fail = undef,
   Optional[Boolean] $ip4_never_default = undef,
 
-  Optional[Enum[dhcp,'link-local',manual,auto,ignore]] $ip6_method = undef,
+  Optional[Enum[disabled,dhcp,'link-local',manual,auto,ignore]] $ip6_method = undef,
   Optional[Variant[Stdlib::IP::Address::V6::CIDR, Array[Stdlib::IP::Address::V6::CIDR]]] $ip6_addresses = undef,
   Optional[Stdlib::IP::Address::V6::Nosubnet] $ip6_gateway = undef,
   Optional[Array[Stdlib::IP::Address::V6::Nosubnet]] $ip6_dns = undef,
