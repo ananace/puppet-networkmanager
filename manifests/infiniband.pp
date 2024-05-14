265c850777df167be57179445d22d574fcecdc31
Ulrik Haugen <ulrik.haugen@liu.se>
2024-05-14 14:21:51 +0200

    Mark ip6_methods as optional in each defined type...
    ... instead of in the Connectionip6method data type.

--- manifests/infiniband.pp
+++ manifests/infiniband.pp
@@ -21,7 +21,7 @@ define networkmanager::infiniband (
   Optional[Boolean] $ip4_may_fail = undef,
   Optional[Boolean] $ip4_never_default = undef,
 
-  Networkmanager::Connectionip6method $ip6_method = undef,
+  Optional[Networkmanager::Connectionip6method] $ip6_method = undef,
   Optional[Variant[Stdlib::IP::Address::V6::CIDR, Array[Stdlib::IP::Address::V6::CIDR]]] $ip6_addresses = undef,
   Optional[Stdlib::IP::Address::V6::Nosubnet] $ip6_gateway = undef,
   Optional[Array[Stdlib::IP::Address::V6::Nosubnet]] $ip6_dns = undef,
